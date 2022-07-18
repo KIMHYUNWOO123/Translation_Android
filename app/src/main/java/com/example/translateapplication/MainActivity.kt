29155ec3f4e4a3839ea359422edcd82f1f479604
khw <wry1004@naver.com>
2022-07-18 18:13:00 +0900

    코루틴 플로우 구현은 했지만ui변경까진 못함

--- app/src/main/java/com/example/translateapplication/MainActivity.kt
+++ app/src/main/java/com/example/translateapplication/MainActivity.kt
@@ -1,26 +1,61 @@
 package com.example.translateapplication
 
+
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
 import android.util.Log
+import androidx.annotation.RequiresApi
+import com.example.translateapplication.coroutine.onTextChange
 import com.example.translateapplication.retrofit.RetrofitManage
 import kotlinx.android.synthetic.main.activity_main.*
-import okio.Utf8
-import java.net.URLEncoder
-
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import kotlin.concurrent.thread
 class MainActivity : AppCompatActivity() {
+    var TAG = "Main"
+    private var myJob: Job = Job()
+    private var myCoroutineScope = CoroutineScope(Dispatchers.IO + myJob)
+    var source = "ko"
+    var target = "en"
+    var data : String? = null
+    var result : String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
-        var TAG = "Main"
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        btn.setOnClickListener {
+//            var text = transText.text.toString()
+//            var source = "ko"
+//            var target = "en"
+//            Log.d(TAG, "onCreate: $text")
+//            var data = RetrofitManage.instance.getTrans(source,target, text)
+//            resultText.text = data
+//        }
 
-        btn.setOnClickListener {
-            var text = transText.text.toString()
-            var source = "ko"
-            var target = "en"
-            Log.d(TAG, "onCreate: $text")
-            var data = RetrofitManage.instance.getTrans(source,target, text)
-            resultText.text = data
+            myCoroutineScope.launch {
+                var flowText = transText.onTextChange()
+                flowText
+                    .debounce(1000)
+                    .filter {
+                        it?.length!! > 0
+                    }
+                    .onEach {
+                        Log.d(TAG, "onCreate: 1")
+                        data = it.toString()
+                        result = RetrofitManage.instance.getTrans(source, target, data!!)
+                    }
+                    .launchIn(this)
+                    delay(2000L)
+                    withContext(Dispatchers.Main){
+                        Log.d(TAG, "onCreate: 2")
+                        resultText.text = result
+                    }
         }
     }
+    override fun onDestroy() {
+        super.onDestroy()
+        myCoroutineScope.cancel()
+    }
+
 }
