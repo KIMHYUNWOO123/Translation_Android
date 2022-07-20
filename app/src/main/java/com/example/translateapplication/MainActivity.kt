350963d8d784eda8eb7f6e1202aba70cc55f0dbb
khw <wry1004@naver.com>
2022-07-20 19:04:20 +0900

    동기화성공;;

--- app/src/main/java/com/example/translateapplication/MainActivity.kt
+++ app/src/main/java/com/example/translateapplication/MainActivity.kt
@@ -4,6 +4,8 @@ package com.example.translateapplication
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 
 import android.util.Log
 import androidx.annotation.RequiresApi
@@ -17,45 +19,50 @@ class MainActivity : AppCompatActivity() {
     var TAG = "Main"
     private var myJob: Job = Job()
     private var myCoroutineScope = CoroutineScope(Dispatchers.IO + myJob)
+    private var myHandler: MyHandler? = null
     var source = "ko"
     var target = "en"
-    var data : String? = null
-    var result : String? = null
+    var data: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        btn.setOnClickListener {
-//            var text = transText.text.toString()
-//            var source = "ko"
-//            var target = "en"
-//            Log.d(TAG, "onCreate: $text")
-//            var data = RetrofitManage.instance.getTrans(source,target, text)
-//            resultText.text = data
-//        }
 
-            myCoroutineScope.launch {
-                var flowText = transText.onTextChange()
-                flowText
-                    .debounce(1000)
-                    .filter {
-                        it?.length!! > 0
-                    }
-                    .onEach {
+        myHandler = MyHandler()
+
+        myCoroutineScope.launch {
+            var flowText = transText.onTextChange()
+            flowText
+                .debounce(1000)
+                .filter {
+                    it?.length!! > 0
+                }
+                .onEach {
                         Log.d(TAG, "onCreate: 1")
                         data = it.toString()
-                        result = RetrofitManage.instance.getTrans(source, target, data!!)
-                    }
-                    .launchIn(this)
-                    delay(2000L)
-                    withContext(Dispatchers.Main){
-                        Log.d(TAG, "onCreate: 2")
-                        resultText.text = result
-                    }
+                        RetrofitManage.instance.getTrans(source, target, data!!, myHandler!!)
+
+                }
+                .launchIn(this)
         }
     }
+
     override fun onDestroy() {
         super.onDestroy()
         myCoroutineScope.cancel()
     }
 
+    inner class MyHandler : Handler() {
+        override fun handleMessage(msg: Message) {
+            super.handleMessage(msg)
+            Log.d(TAG, "handleMessage:${msg.obj}")
+            if(msg.obj != null) {
+                when (msg.what) {
+                    1 -> {
+                        resultText.text = msg.obj.toString()
+                    }
+
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/translateapplication/retrofit/RetrofitManage.kt
+++ app/src/main/java/com/example/translateapplication/retrofit/RetrofitManage.kt
@@ -1,6 +1,8 @@
 package com.example.translateapplication.retrofit
 
+import android.os.Message
 import android.util.Log
+import com.example.translateapplication.MainActivity
 import com.example.translateapplication.utils.Data
 import retrofit2.Call
 import retrofit2.Callback
@@ -8,27 +10,28 @@ import retrofit2.Response
 
 
 class RetrofitManage {
-    var data : String? = null
     val TAG = "RetrofitManage"
     companion object{
         var instance = RetrofitManage()
     }
     private var iRetrofit: IRetrofit? = RetrofitClient.getRetrofit(Data.BASEURL)?.create(IRetrofit::class.java)
-    fun getTrans(source : String, target: String, text : String ) : String?{
+    fun getTrans(source : String, target: String, text : String, myHandler : MainActivity.MyHandler ){
 
         var call = iRetrofit?.getTranslate(Data.clientId,Data.clientSecret,source, target, text)
         Log.d(TAG, "getTrans: $text")
         call?.enqueue(object : Callback<ReturnData>{
             override fun onResponse(call: Call<ReturnData>, response: Response<ReturnData>) {
                 Log.d(TAG, "onResponse: ${response.body()!!.message.result.translatedText}")
-                data = response.body()!!.message.result.translatedText
+                var message = Message.obtain()
+                message.what = 1
+                message.obj = response.body()!!.message.result.translatedText
+                myHandler.sendMessage(message)
             }
 
             override fun onFailure(call: Call<ReturnData>, t: Throwable) {
                 Log.d(TAG, "onFailure: $t ")
             }
         })
-        return data
     }
 
 }
