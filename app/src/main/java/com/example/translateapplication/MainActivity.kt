1f748d649ce8f8b534d25dec734452f3dad0ac55
khw <wry1004@naver.com>
2022-07-23 17:16:29 +0900

    음성출력..까짛ㅎ

--- app/src/main/java/com/example/translateapplication/MainActivity.kt
+++ app/src/main/java/com/example/translateapplication/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.translateapplication
 
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Message
+import android.speech.tts.TextToSpeech
 
 import android.util.Log
 import android.widget.Toast
@@ -13,6 +15,7 @@ import com.example.translateapplication.retrofit.RetrofitManage
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
+import java.util.*
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
@@ -23,25 +26,30 @@ class MainActivity : AppCompatActivity() {
     var source: String? = null
     var target: String? = null
     var data: String? = null
+    private var tts : TextToSpeech? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         myHandler = MyHandler()
         transText.isEnabled = false
-
+        initTextToSpeech()
+        btn_speak.setOnClickListener {
+            if(data != null){
+                Log.d(TAG, "onCreate: 음성출력")
+                ttsSpeak(data!!)
+            }
+        }
         thread(true) {
-            while (true) {
+            while (true){
                 if (source != null && target != null && source != target) {
                     runOnUiThread {
                         transText.isEnabled = true
                     }
-                    Log.d(TAG, "onCreate: 열림")
                 }
-                if(source == target){
+                if(source == target && source != null && transText.isEnabled){
                     runOnUiThread {
                         transText.isEnabled = false
                     }
-                    Log.d(TAG, "onCreate: 닫힘")
                 }
             }
         }
@@ -67,7 +75,7 @@ class MainActivity : AppCompatActivity() {
             flowText
                 .debounce(1000)
                 .filter {
-                    it?.length!! > 0 && source != null && target != null && source != target
+                    it?.length!! > 0
                 }
                 .onEach {
                     Log.d(TAG, "onCreate: 호출")
@@ -76,6 +84,25 @@ class MainActivity : AppCompatActivity() {
                 }
                 .launchIn(this)
         }
+    }
+    private fun initTextToSpeech(){
+        Log.d(TAG, "initTextToSpeech: 함수실행")
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+            return
+        }
+        tts = TextToSpeech(applicationContext, TextToSpeech.OnInitListener {
+            if(it == TextToSpeech.SUCCESS){
+                var result = tts?.setLanguage(Locale.ENGLISH)
+                if(result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED){
+                    Log.d(TAG, "initTextToSpeech: 초기화 성공")
+                    return@OnInitListener
+                }
+            }
+        })
+    }
+
+    private fun ttsSpeak(text : String){
+        tts?.speak(text,TextToSpeech.QUEUE_ADD, null, null)
     }
     override fun onDestroy() {
         super.onDestroy()
